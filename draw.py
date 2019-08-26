5cceb6ac6a30522320c675c961daf1bcc002d009
A.Sikorski <sikorski.aps@gmail.com>
2019-08-26 22:45:11 +0200

    Slices visualisation, basic version

--- draw.py
+++ draw.py
@@ -2,14 +2,16 @@ import matplotlib.pyplot as plt
 import matplotlib.image as mpl_image
 from matplotlib.patches import Polygon
 import matplotlib.patheffects as PathEffects
+import numpy as np
 
 
-def prepare(background_image_filename):
+def prepare(background_image_filename, draw=True):
     image = mpl_image.imread(background_image_filename)
     plt.close(plt.gcf())
     plt.figure(figsize=(image.shape[1] / 100, image.shape[0] / 100))
     plt.subplots_adjust(top=1, bottom=0, right=1, left=0, hspace=0, wspace=0)
-    plt.imshow(image)
+    if draw:
+        plt.imshow(image)
     plt.axis("off")
     plt.xlim([0, image.shape[1]])
     plt.ylim([image.shape[0], 0])
@@ -35,10 +37,37 @@ def text(*args, effects=None, **kwargs):
         txt.set_path_effects([PathEffects.withStroke(**effects)])
 
 
+def offset(x, y, o):
+    """ Offset coordinates given by array x,y by o """
+    X = np.c_[x, y].T
+    m = np.array([[0, -1], [1, 0]])
+    R = np.zeros_like(X)
+    S = X[:, 2:] - X[:, :-2]
+    R[:, 1:-1] = np.dot(m, S)
+    R[:, 0] = np.dot(m, X[:, 1] - X[:, 0])
+    R[:, -1] = np.dot(m, X[:, -1] - X[:, -2])
+    On = R / np.sqrt(R[0, :] ** 2 + R[1, :] ** 2) * o
+    Out = On + X
+    return Out[0, :], Out[1, :]
+
+
+def offset_curve(x, y, o):
+    """ Offset array x,y in data coordinates
+        by o in points """
+    ax = plt.gca()
+    trans = ax.transData.transform
+    inv = ax.transData.inverted().transform
+    X = np.c_[x, y]
+    Xt = trans(X)
+    xto, yto = offset(Xt[:, 0], Xt[:, 1], o * 100 / 72.)
+    Xto = np.c_[xto, yto]
+    Xo = inv(Xto)
+    return Xo[:, 0], Xo[:, 1]
+
+
 def save_as(*args, **kwargs):
     plt.savefig(*args, **kwargs)
 
 
 def show():
     plt.show()
-
--- scripts/slices.py
+++ scripts/slices.py
@@ -6,38 +6,57 @@ import geomanip
 import draw
 import os
 import sndlib
+import re
 
 
-class DataInfo:
+def linear_map(value, a, b, new_a, new_b):
+    ret = new_a + (value - a) / (b - a) * (new_b - new_a)
+    return ret
+
+
+class SlicesAnalyzer:
     """
         transponders -> key - int, value -> (x, y) x - bitrate, y - slices used
         number_of_vertices -> amount of cities
     """
 
-    def __init__(self, result_file, paths_file):
+    def __init__(self, result_file, paths_file, nvertices, nedges, nslices, npaths, transponders=None):
+        """
+
+        :param result_file: .out file
+        :param paths_file: .dat_file
+        :param transponders: dictionairy of usage
+        :param nslices: number of slices
+        :param npaths: number of paths
+        :param nvertices: number of vertices
+        :param nedges: number of edges
+        """
         # demanded value, unused
         self.demand = 1600
 
         # transponder type -> (bitrate, used slices)
-        self.transponders = {1: (40, 1), 2: (100, 1), 3: (200, 1)}
+        self.transponders = transponders if transponders is not None else {1: (40, 1), 2: (100, 1), 3: (200, 1)}
 
         # summary number of slices
-        self.number_of_slices = 384
+        self.number_of_slices = nslices  # 384
 
         # number of paths per demand
-        self.number_of_paths = 3
+        self.number_of_paths = npaths  # 3
 
         # number of nodes in the network
-        self.number_of_vertices = 17
+        self.number_of_vertices = nvertices  # 17
 
         # number of edges in the network
-        self.number_of_edges = 26
+        self.number_of_edges = nedges  # 26
 
-        # filename with data for X(t, n, n', p, s)
+        # filename with data for X(t, n, n', p, s) and FREQB (which slices are in which band)
         self.file = result_file
 
         # filename with paths data
         self.paths_file = paths_file
+
+        # slices in bands
+        self.bands = {}
 
         vertices_pairs = filter(lambda v: v[0] != v[1], np.ndindex(self.number_of_vertices, self.number_of_vertices))
         self.paths_dict = {verts: [list() for _ in range(self.number_of_paths)] for verts in vertices_pairs}
@@ -52,7 +71,7 @@ class DataInfo:
 
     def upload_data(self):
         """
-        Load X(t, n, n', p, s) data.
+        Load X(t, n, n', p, s).
 
         :return: five dimensional X(...) array
         """
@@ -68,7 +87,7 @@ class DataInfo:
                     # if int(x) == 1:
                     #     ones += 1
             self.file.readline()
-        # print(ones)
+
         return self.X
 
     def statistics(self):
@@ -97,6 +116,19 @@ class DataInfo:
             self.paths_file.readline()
         return self.paths_dict
 
+    def upload_bands_data(self):
+        line = ''
+        while not line.startswith('set FREQB['):
+            line = self.paths_file.readline()
+
+        while line.startswith('set FREQB['):
+            match = re.search(r'set FREQB\[(\d+)\]:= ((\d+\s?)+)', line)
+            band, numbers = int(match.group(1)), [int(x) for x in match.group(2).split()]
+            # print(band, numbers)
+            for slice_num in numbers:
+                self.bands[slice_num] = band
+            line = self.paths_file.readline()
+
     def compute_used_slices(self):
         """
         Compute used slices.
@@ -112,24 +144,45 @@ class DataInfo:
                     slices_in_edges[edge] += used_slices
         return slices_in_edges
 
+    def compute_used_slices_in_bands(self):
+        """
+        Compute used slices.
+
+        :return: dictionary of slice usage (key: edge ID, value: dict of used slices)
+        """
+        slices_in_edges = defaultdict(dict)
+        for t, n1, n2, p, s in np.ndindex(self.X.shape):
+            if n1 != n2:
+                used_slices = self.X[t, n1, n2, p, s] * self.transponders[t + 1][1]
+                band = self.bands[s + 1]
+
+                for edge in self.paths_dict[(n2, n1)][p]:
+                    slices_in_edges[edge][band] = slices_in_edges[edge].get(band, 0) + used_slices
+        return slices_in_edges
+
 
 if __name__ == '__main__':
-    # load slices usage data and compute percentage usage
-    # slice_usage = {}
     with open("../data/ger3.out") as file, open("../data/ger3.dat") as path_file:
-        data_info = DataInfo(result_file=file, paths_file=path_file)
+        data_info = SlicesAnalyzer(
+            result_file=file, paths_file=path_file,
+            nvertices=17,
+            nedges=26,
+            npaths=3,
+            nslices=384
+        )
         data_info.upload_data()
         data_info.upload_paths()
+        data_info.upload_bands_data()
         slices_in_edges = data_info.compute_used_slices()
+        slices_in_bands = data_info.compute_used_slices_in_bands()
         slice_usage = {k: (v / data_info.number_of_slices * 100) for (k, v) in slices_in_edges.items()}
-        print('slices in edges:')
-        pprint(slices_in_edges)
-        print(sum(slices_in_edges.values()))
+        print('slices in edges (by band):')
+        pprint(slices_in_bands)
 
     # settings for appropriate mercator displaying
     net = sndlib.UndirectedNetwork.load_native('../data/nobel-germany.txt')
     points = net.get_list_of_coordinates()
-    projection = geomanip.MercatorProjection.from_points(points=points, map_size=(768, 1024), padding=125)
+    projection = geomanip.MercatorProjection.from_points(points=points, map_size=(768, 1024), padding=75)
     net.add_pixel_coordinates(projection)
 
     status = mapbox.get_map_as_file(
@@ -141,20 +194,39 @@ if __name__ == '__main__':
     )
     print('Map ' + ('' if status else 'not ') + 'dowloaded')
 
-    draw.prepare('../data/map-germany.png')
-    for node in net.nodes:
-        draw.text(node.x, node.y, node.name, fontsize=6, color='black', weight='bold', horizontalalignment='center')
+    draw.prepare('../data/map-germany.png', draw=True)
 
+    linewidth = 3
+    nslices_L = sum(1 for band in data_info.bands.values() if band == 1)
+    nslices_C = sum(1 for band in data_info.bands.values() if band == 2)
     for u, v, d in net.edges(data=True):
-        sx, sy = net.edge_middle_point(u, v, pixel_value=True)
-        draw.line(u.x, u.y, v.x, v.y, marker='o', color='gray')
-        draw.text(
-            sx, sy,
-            f'{slices_in_edges[d["edge_id"]]}',
-            fontsize=7,
-            color='navy',
-            weight='bold',
-            horizontalalignment='center'
-        )
+        eid = d['edge_id']
+        C_slices = slices_in_bands[eid][1]
+        L_slices = slices_in_bands[eid][2]
+        C_color = (0, 0, 1, linear_map(C_slices, 0, nslices_C, 0.0, 1))
+        L_color = (1, 0, 0, linear_map(L_slices, 0, nslices_L, 0.0, 1))
+
+        oxs, oys = draw.offset_curve([u.x, v.x], [u.y, v.y], linewidth)
+        # draw.line(u.x, u.y, v.x, v.y, marker='', color=(1.0, 0.0, 0.0, L_slices / 384), linewidth=lw)
+        # draw.line(u.x + xtr, u.y + ytr, v.x + xtr, v.y + ytr, marker='', color='orange', linewidth=lw / 2)
+
+        draw.line(oxs[0], oys[0], oxs[1], oys[1], color=C_color, linewidth=linewidth)
+        draw.line(u.x, u.y, v.x, v.y, color=L_color, linewidth=linewidth)
+
+        # draw.line(u.x, u.y, v.x, v.y, marker='o', color='gray', linewidth=2)
+        # sx, sy = net.edge_middle_point(u, v, pixel_value=True)
+        # draw.text(
+        #     sx, sy,
+        #     f'L: {L_slices}, C: {C_slices}',
+        #     fontsize=8,
+        #     color='navy',
+        #     weight='bold',
+        #     horizontalalignment='center'
+        # )
+
+    for node in net.nodes:
+        text = node.name
+        draw.text(node.x, node.y, text, fontsize=8, color='black', weight='bold', horizontalalignment='center')
 
     draw.show()
+    # draw.save_as('../output/lc_band.png')

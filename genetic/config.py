cc2f6d6df4a971317ce070eaab53ec25f1fd41e9
kacperwnuk <kacperwnuk97@o2.pl>
2019-10-30 10:12:27 +0100

    Refactor of config file

--- genetic/config.py
+++ genetic/config.py
@@ -6,36 +6,36 @@ import geneticlib
 import time
 import sndlib
 import timer
+import utils
 import yen
 import genetic.transponders as tconfiger
 # import genetic.transponder_config as t_config
 from DataAnalyzis.analyzer import DataInfo
 from geneticlib import Individual
 
-net_name = 'polska'
+net_name = 'abilene'
+dat_source_prefix = 'usa'
 net = sndlib.create_undirected_net(net_name, calculate_distance=True, calculate_reinforcement=True, calculate_ila=True)
+
+K = 3  # number of predefined paths
+# predefined_paths = yen.ksp_all_nodes(net, nx.astar_path, heuristic_fun=dist, k=K)
+# predefined_paths = get_kozdro_paths()
+intensity = 0.01
+intensity_str = f"{intensity}".replace(".", "")
+predefined_paths = utils.get_predefined_paths(network_filename=f"data/sndlib/json/{net_name}/{net_name}.json",
+                                              dat_filename=f"data/{dat_source_prefix}{intensity_str}.dat", npaths=K)
+
+# demands = {key: DEMAND for key in predefined_paths}
+# transponders_config = {DEMAND: t_config.create_config([(40, 4), (100, 4), (200, 8), (400, 12)], DEMAND, 3)}
+t_config_file = 'data/transponder_configs_ip_5.json'
+transponders_config = tconfiger.load_config(t_config_file)
+demands = {key: math.ceil(value * intensity) for key, value in net.demands.items()}
 
 
 @lru_cache(maxsize=1024)
 def dist(a, b):
     return sndlib.calculate_haversine_distance_between_each_node(net)[a][b]
 
-
-def get_kozdro_paths():
-    with open("data\pol10.dat") as path_file, open("data\cities.txt") as cities_file, open(
-            'data\edges.txt') as edges_file:
-        data_info = DataInfo(result_file=None, paths_file=path_file, cities_file=cities_file, edges_file=edges_file)
-        data_info.upload_paths()
-
-        # for key, values in data_info.paths_dict.items():
-        #     data_info.paths_dict[key] = [values[0]]
-            # print(f"{key}: {values}\n")
-    return data_info.paths_dict
-
-
-K = 3  # number of predefined paths
-# predefined_paths = yen.ksp_all_nodes(net, nx.astar_path, heuristic_fun=dist, k=K)
-predefined_paths = get_kozdro_paths()
 
 slices_usage = {
     0: 1,
@@ -68,20 +68,13 @@ bands = [(0, 191), (192, 383)]  # ranges of slices per band
 POP_SIZE = 100
 NEW_POP_SIZE = 10
 
-# demands = {key: DEMAND for key in predefined_paths}
-# transponders_config = {DEMAND: t_config.create_config([(40, 4), (100, 4), (200, 8), (400, 12)], DEMAND, 3)}
-t_config_file = 'data/transponder_configs_ip_4.json'
-transponders_config = tconfiger.load_config(t_config_file)
-intensity = 10
-demands = {key: math.ceil(value * intensity) for key, value in net.demands.items()}
-
 CPB = 80
 GSPB = 20  # Gene swap probability
 MPB = 50
 CPPB = 1  # Change path probability
 GA_ITERATIONS = 75
 
-HILL_ITERATIONS = 3000
+HILL_ITERATIONS = 4500
 
 tools = geneticlib.Toolkit(crossing_probability=CPB, mutation_probability=MPB)
 tools.set_fitness_weights(weights=(-1,))
@@ -122,7 +115,3 @@ def save_result(best_result: Individual, file_name: str):
 
     with open(f'results/{file_name}', mode='w') as file:
         file.write(result)
-
-
-
-

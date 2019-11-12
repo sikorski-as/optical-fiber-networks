f07832eea41b3f40b695947a64c7e13a21cb167b
kacperwnuk <kacperwnuk97@o2.pl>
2019-11-12 20:43:31 +0100

    Version before solving circular imports

--- structure.py
+++ structure.py
@@ -1,10 +1,11 @@
 import copy
-import operator
 import random
+import abc
 from collections import defaultdict
 import bitstring
 import geneticlib
 import utils
+
 from main_config import e, l, bandwidth, P, OSNR, h, freq, W, V
 import main_config
 
@@ -18,9 +19,9 @@ class Slice:
         return f"{self.value}"
 
 
-class Chromosome:
+class Chromosome(abc.ABC):
 
-    def __init__(self, net, predefined_paths, transponders_config, bands, slices_usage, transponders_cost):
+    def __init__(self, net, predefined_paths, transponders_config, bands, transponder_slices_usage, transponders_cost):
         """
         :param predefined_paths: {} key (city1, city2)
         :param transponders_config: {} key demand
@@ -33,7 +34,7 @@ class Chromosome:
         self.transponders_config = transponders_config
         self.demands = net._demands
         self.bands = bands
-        self.slices_usage = slices_usage
+        self.transponder_slices_usage = transponder_slices_usage
         self.transponders_cost = transponders_cost
         self._structure = self._create_structure()
         self.slices_overflow = None
@@ -51,6 +52,152 @@ class Chromosome:
             structure[key] = self._create_gene(key)
         return structure
 
+    def _use_slices(self, transponder_slices_used, path_slices_utilization, bands):
+        """
+        Finds empty space for transponder.
+        :param transponder_slices_used: how many empty slices in a row needs to be found
+        :param path_slices_utilization: slices already used by previous transponders
+        :param bands:
+        :return: slices used or None if there is not enough space
+        """
+        empty_space = 0
+        for i, bit in enumerate(path_slices_utilization):
+            if i + transponder_slices_used > bands[0][1] and i < bands[1][0]:
+                empty_space = 0
+                continue
+            if bit == 0:
+                empty_space += 1
+                if empty_space == transponder_slices_used:
+                    return i - transponder_slices_used + 1, i
+            else:
+                empty_space = 0
+
+        return None
+
+    def _allocate_slices(self, subgenes):
+        slices_usage_map = defaultdict(lambda: bitstring.BitArray(self.bands[1][1]))
+        sorted_subgenes = self.prepare_subgenes(subgenes)
+        slices_overflow = 0
+        edges_used = defaultdict(set)
+        bands_usage = defaultdict(int)  # counts edges used in each band
+
+        for subgene in sorted_subgenes:
+            path_slices_utilization = None
+            for edge in utils.pairwise(subgene[1]):
+                edge = tuple(sorted(edge))
+                if path_slices_utilization is not None:
+                    path_slices_utilization = path_slices_utilization | slices_usage_map[edge]
+                else:
+                    path_slices_utilization = slices_usage_map[edge]
+            slices_used_by_subgene = self.calculate_slices_demand(subgene[0])
+            slices_used = self._use_slices(slices_used_by_subgene, path_slices_utilization,
+                                           self.bands)
+            if slices_used:
+                for edge in utils.pairwise(subgene[1]):
+                    edge = tuple(sorted(edge))
+                    slices_usage_map[edge].set(1, [i for i in range(slices_used[0], slices_used[1] + 1)])
+                    band = 0 if slices_used[0] < self.bands[0][1] else 1
+                    if edge not in edges_used[band]:
+                        bands_usage[band] += 1
+                        edges_used[band].add(edge)
+                subgene[2].value = slices_used[0]
+            else:
+                slices_overflow += self.calculate_slices_demand(subgene[0])
+                # jaki slice ustawić jak sie nie miesci?
+
+        return slices_overflow, bands_usage
+
+    @abc.abstractmethod
+    def prepare_subgenes(self, subgenes):
+        return
+
+    @abc.abstractmethod
+    def calculate_slices_demand(self, transponders):
+        return
+
+    @abc.abstractmethod
+    def _create_gene(self, key):
+        return
+
+    @abc.abstractmethod
+    def _choose_slice(self, transponders):
+        return
+
+    @abc.abstractmethod
+    def get_transponder_usage_cost(self):
+        return
+
+    @abc.abstractmethod
+    def _check_power(self):
+        return
+
+    def __repr__(self):
+        return f"PO:{self.power_overflow} SO:{self.slices_overflow}"
+
+    @property
+    def genes(self):
+        return self._structure
+
+
+class OneSubgeneChromosome(Chromosome):
+    "Warning! Not working for big demands!"
+
+    def __init__(self, net, predefined_paths, transponders_config, bands, slices_usage, transponders_cost):
+        super().__init__(net, predefined_paths, transponders_config, bands, slices_usage, transponders_cost)
+
+    def _create_gene(self, key):
+        demand = self.demands[key]
+        transponder_config = random.choice(self.transponders_config[demand])
+        return transponder_config, random.choice(self.predefined_paths[key]), Slice(
+            self._choose_slice(transponder_config))
+
+    def _choose_slice(self, transponders):
+        slices_used = self.calculate_slices_demand(transponders)
+        begin, end = random.choice(self.bands)
+        return random.randrange(begin, end - slices_used)
+
+    def prepare_subgenes(self, subgenes):
+        return sorted(subgenes, key=lambda x: len(x[1]), reverse=True)
+
+    def calculate_slices_demand(self, transponders):
+        return sum([amount * self.transponder_slices_usage[transponder_type] for transponder_type, amount in
+                    enumerate(transponders)])
+
+    def get_transponder_usage_cost(self):
+        cost = 0
+        for subgene in self.genes.values():
+            band = 0 if subgene[2].value < self.bands[0][1] else 1
+            cost += sum([amount * self.transponders_cost[transponder_type, band] for transponder_type, amount in
+                         enumerate(subgene[0])])
+        return cost
+
+    def _check_power(self):
+        power_overflow = 0
+        for transponders, path, slice in self.genes.values():
+            for transponder_type, amount in enumerate(transponders):
+                band = 0 if slice.value <= self.bands[0][1] else 1
+                total = 0
+                for edge in utils.pairwise(path):
+                    total += self.net.edges[edge]['ila'] * (
+                            pow(e, l[band] * self.net.edges[edge]['distance'] /
+                                (1 + self.net.edges[edge]['ila'])) + V - 2)
+                    total += pow(e,
+                                 l[band] * self.net.edges[edge]['distance'] / (
+                                         1 + self.net.edges[edge]['ila'])
+                                 ) + W - 2
+                total *= h * freq[band] * OSNR[transponder_type] * bandwidth[transponder_type] * amount
+                total = round(total, 9)
+                if total > P:
+                    power_overflow += total
+        self.power_overflow = power_overflow
+        return power_overflow
+
+
+class MultipleSubgeneChromosome(Chromosome):
+
+    def __init__(self, net, predefined_paths, transponders_config, bands, slices_usage, transponders_cost):
+        super().__init__(net, predefined_paths, transponders_config, bands, slices_usage, transponders_cost)
+
     def _create_gene(self, key):
         demand = self.demands[key]
         transponder_config = random.choice(self.transponders_config[demand])
@@ -63,27 +210,57 @@ class Chromosome:
         return L
 
     def _choose_slice(self, transponder_type):
-        slices_used = self.slices_usage[transponder_type]
+        slices_used = self.transponder_slices_usage[transponder_type]
         begin, end = random.choice(self.bands)
         return random.randrange(begin, end - slices_used)
 
-    def __repr__(self):
-        return f"PO:{self.power_overflow} SO:{self.slices_overflow}"
+    def prepare_subgenes(self, genes):
+        flatten_subgenes = [subgene for gene in genes for subgene in gene]
+        return sorted(flatten_subgenes, key=lambda x: len(x[1]), reverse=True)
 
-    @property
-    def genes(self):
-        return self._structure
+    def calculate_slices_demand(self, transponder_type):
+        return self.transponder_slices_usage[transponder_type]
+
+    def get_transponder_usage_cost(self):
+        total_cost = 0
+        for gene in self.genes.values():
+            for subgene in gene:
+                band = 0 if subgene[2].value < self.bands[0][1] else 1
+                total_cost += self.transponders_cost[subgene[0], band]
+        return total_cost
+
+    def _check_power(self):
+        power_overflow = 0
+        for subgenes in self.genes.values():
+            for transponder_type, path, slice in subgenes:
+                band = 0 if slice.value <= self.bands[0][1] else 1
+                total = 0
+                for edge in utils.pairwise(path):
+                    total += self.net.edges[edge]['ila'] * (
+                            pow(e, l[band] * self.net.edges[edge]['distance'] /
+                                (1 + self.net.edges[edge]['ila'])) + V - 2)
+                    total += pow(e,
+                                 l[band] * self.net.edges[edge]['distance'] / (
+                                         1 + self.net.edges[edge]['ila'])
+                                 ) + W - 2
+                total *= h * freq[band] * OSNR[transponder_type] * bandwidth[transponder_type]
+                total = round(total, 9)
+                if total > P:
+                    power_overflow += total
+        self.power_overflow = power_overflow
+        return power_overflow
 
 
-def create_individual():
+def create_individual(ChromosomeType):
     """
     Creates individual without fitness value.
     :return: Individual
     """
-    adapted_predefined_paths = {key: [value[1] for value in values] for key, values in main_config.predefined_paths.items()}
+    adapted_predefined_paths = {key: [value[1] for value in values] for key, values in
+                                main_config.predefined_paths.items()}
     individual = geneticlib.Individual(
-        Chromosome(main_config.net, adapted_predefined_paths, main_config.transponders_config, main_config.bands,
-                   main_config.slices_usage, main_config.transponders_cost))
+        ChromosomeType(main_config.net, adapted_predefined_paths, main_config.transponders_config, main_config.bands,
+                       main_config.slices_usage, main_config.transponders_cost))
     return individual
 
 
@@ -97,14 +274,7 @@ def mutate_gene(gene, predefined_paths):
         gene[i] = change_path(subgene, predefined_paths)
 
 
-def fitness(chromosome):
-    cost = chromosome.transponders_cost
-    total_cost = 0
-
-    # version 1
-    slices_overflow, bands_usage = _allocate_slices(chromosome.genes.values(), chromosome.bands,
-                                                    chromosome.slices_usage)
-    chromosome.slices_overflow = slices_overflow
+def fitness(chromosome: Chromosome):
     # version 2
     # slices_overflow = 0
     # slices_usage = defaultdict(set)
@@ -116,97 +286,19 @@ def fitness(chromosome):
     #                     slices_overflow += 1
     #                 else:
     #                     slices_usage[edge].add(slice)
+    total_cost = 0
 
-    for gene in chromosome.genes.values():
-        for subgene in gene:
-            band = 0 if subgene[2].value < chromosome.bands[0][1] else 1
-            total_cost += cost[subgene[0], band]
+    # version 1
 
-    power_overflow = _check_power(chromosome)
+    slices_overflow, bands_usage = chromosome._allocate_slices(chromosome.genes.values())
+    chromosome.slices_overflow = slices_overflow
+    total_cost += chromosome.get_transponder_usage_cost()
+
+    power_overflow = chromosome._check_power()
     # print(slices_overflow)
     # print(total_cost)
     amplifiers_cost = sum([main_config.b_cost[key] * value for key, value in bands_usage.items()])
     return total_cost * pow(2.72, power_overflow * 100) + pow(slices_overflow, 2) + amplifiers_cost
-
-
-def _check_power(chromosome: Chromosome):
-    # node_reinforcement = sndlib.calculate_reinforcement_for_each_node(chromosome.net)
-
-    power_overflow = 0
-    for subgene in chromosome.genes.values():
-        for transponder_type, path, slice in subgene:
-            band = 0 if slice.value <= chromosome.bands[0][1] else 1
-            total = 0
-            for edge in utils.pairwise(path):
-                total += chromosome.net.edges[edge]['ila'] * (pow(e, l[band] * chromosome.net.edges[edge]['distance'] /
-                                                                  (1 + chromosome.net.edges[edge]['ila'])) + V - 2)
-                total += pow(e, l[band] * chromosome.net.edges[edge]['distance'] / (1 + chromosome.net.edges[edge]['ila'])
-                             ) + W - 2
-            total *= h * freq[band] * OSNR[transponder_type] * bandwidth[transponder_type]
-            total = round(total, 9)
-            if total > P:
-                power_overflow += total
-    chromosome.power_overflow = power_overflow
-    return power_overflow
-
-
-def _allocate_slices(genes, bands, transponder_slices_usage):
-    slices_usage = defaultdict(lambda: bitstring.BitArray(bands[1][1]))
-    flatten_subgenes = [subgene for gene in genes for subgene in gene]
-    sorted_subgenes = sorted(flatten_subgenes, key=lambda x: len(x[1]), reverse=True)
-    # sorted_subgenes = sorted(flatten_subgenes, key=lambda x: x[0], reverse=True)
-    # sorted_subgenes = sorted(flatten_subgenes, key=lambda x: (x[0], len(x[1])), reverse=True)
-    slices_overflow = 0
-    edges_used = defaultdict(set)
-    bands_usage = defaultdict(int)  # counts edges used in each band
-
-    for subgene in sorted_subgenes:
-        path_slices_utilization = None
-        for edge in utils.pairwise(subgene[1]):
-            edge = tuple(sorted(edge))
-            if path_slices_utilization is not None:
-                path_slices_utilization = path_slices_utilization | slices_usage[edge]
-            else:
-                path_slices_utilization = slices_usage[edge]
-        slices_used = _use_slices(transponder_slices_usage[subgene[0]], path_slices_utilization, bands)
-        if slices_used:
-            for edge in utils.pairwise(subgene[1]):
-                edge = tuple(sorted(edge))
-                slices_usage[edge].set(1, [i for i in range(slices_used[0], slices_used[1] + 1)])
-                band = 0 if slices_used[0] < main_config.bands[0][1] else 1
-                if edge not in edges_used[band]:
-                    bands_usage[band] += 1
-                    edges_used[band].add(edge)
-            subgene[2].value = slices_used[0]
-        else:
-            slices_overflow += transponder_slices_usage[subgene[0]]
-            # jaki slice ustawić jak sie nie miesci?
-
-
-    return slices_overflow, bands_usage
-
-
-def _use_slices(transponder_slices_used, path_slices_utilization, bands):
-    """
-    Finds empty space for transponder.
-    :param transponder_slices_used: how many empty slices in a row needs to be found
-    :param path_slices_utilization: slices already used by previous transponders
-    :param bands:
-    :return: slices used or None if there is not enough space
-    """
-    empty_space = 0
-    for i, bit in enumerate(path_slices_utilization):
-        if i + transponder_slices_used > bands[0][1] and i < bands[1][0]:
-            empty_space = 0
-            continue
-        if bit == 0:
-            empty_space += 1
-            if empty_space == transponder_slices_used:
-                return i - transponder_slices_used + 1, i
-        else:
-            empty_space = 0
-
-    return None
 
 
 def compare(individual: geneticlib.Individual):
@@ -234,5 +326,4 @@ def random_neighbour_ksize(individual: geneticlib.Individual, k):
     main_config.tools.calculate_fitness_values([neighbour], [fitness])
     print(f"{neighbour.chromosome} {neighbour.values[0]} {k}")
     return neighbour
-
 

0c8a935d05ddef6b0ee9df6fc20ef307f9771eed
kacperwnuk <kacperwnuk97@o2.pl>
2020-01-21 21:24:26 +0100

    Wyłączenie pobierania bazy sndlib

--- deploy.sh
+++ deploy.sh
@@ -3,5 +3,5 @@ source venv/bin/activate
 pip3 install setuptools
 pip3 install -r requirements.txt
 mkdir -p results
-cd data
-git clone https://github.com/sikorski-as/sndlib.git
+#cd data
+#git clone https://github.com/sikorski-as/sndlib.git
